ae41346a6386c3b1da7fc590eba1a0a40eea5f6c
Rayhan Egar <rresnn@gmail.com>
2024-09-24 05:50:09 +0700

    M4

--- app/src/main/java/com/rayhanegar/papbm2/MainActivity.kt
+++ app/src/main/java/com/rayhanegar/papbm2/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.rayhanegar.papbm2
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -19,10 +21,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rayhanegar.papbm2.ui.theme.PapbM2Theme
@@ -50,6 +55,14 @@ fun MyScreen() {
     var inputNameText = remember { mutableStateOf("") }
     var nimText = remember { mutableStateOf("") }
     var inputNimText = remember { mutableStateOf("") }
+
+    val isButtonEnabled = remember(inputNameText, inputNimText) {
+        derivedStateOf {
+            inputNameText.value.isNotBlank() && inputNimText.value.isNotBlank()
+        }
+    }
+
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -87,8 +100,20 @@ fun MyScreen() {
 
         Button(onClick = {
             nameText.value = inputNameText.value
-            nimText.value = inputNimText.value
-        })
+            nimText.value = inputNimText.value },
+            enabled = isButtonEnabled.value,
+            modifier = Modifier.pointerInput(Unit){
+                detectTapGestures(
+                    onLongPress = {
+                        Toast
+                            .makeText(
+                                context,
+                                "Hello, ${inputNameText.value}! Your NIM is ${inputNimText.value}", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                )
+            }
+        )
         { Text(text = "Pop",
                 style = MaterialTheme.typography.labelSmall)
         }
